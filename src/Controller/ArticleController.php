168a77cee0859f857d2896dc91375494d15ff390
Olivier Valette <olivier.valette@spi10.com>
2018-11-24 21:47:39 +0100

    Install routing with annotations
    and .htaccess for apache

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page already!');
+    }
+    
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: %s',
+            $slug
+        ));
+    }
+}
